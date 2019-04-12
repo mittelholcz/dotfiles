dcf0b42cc9142b3eaa3b230e9990acbfa8ee4a1e
mittelholcz <dev.mittelholcz@gmail.com>
2019-04-12 16:39:28 +0200

    wip

--- scripts/bash_prompt.sh
+++ scripts/bash_prompt.sh
@@ -119,51 +119,24 @@ function git_status() {
 		exit 0
 	fi
 
+    res=''
+
     # get branch or commit hash
     local branch=$(git status --branch 2> /dev/null | head -n1 | sed -r 's/.*\s(\w+)$/\1/')
 	if [ "${branch}" ] ; then
         local stat="$(git status --porcelain | grep -o '^..')"
-        # status=`git status 2>&1 | tee`
-        # dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
-        # untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
-        # ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
-        # newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
-        # renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
-        # deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
-        # bits=''
-        local res=''
-        if [ -z "${stat}" ] ; then
-            res=$(colortext green "${branch}")
+        local col='green'
+        if [ "${stat}" ] ; then
+            if egrep -q '^.[AMD?]' <<<${stat} ; then
+                col='red'
+            else
+                col='yellow'
+            fi
         fi
-        if [ "${renamed}" == "0" ]; then
-            bits=">${bits}"
-        fi
-        if [ "${ahead}" == "0" ]; then
-            bits="*${bits}"
-        fi
-        if [ "${newfile}" == "0" ]; then
-            bits="+${bits}"
-        fi
-        if [ "${untracked}" == "0" ]; then
-            bits="?${bits}"
-        fi
-        if [ "${deleted}" == "0" ]; then
-            bits="x${bits}"
-        fi
-        if [ "${dirty}" == "0" ]; then
-            bits="!${bits}"
-        fi
-        if [ ! "${bits}" == "" ]; then
-            STAT=" ${bits}"
-        fi
-		# echo " (${branch}${STAT})"
-        if [ "${res}" ] ; then
-            res=" (${res})"
-        fi
-        echo -n "${res}"
-	else
-		echo ""
+        res=$(colortext ${col} ${branch})
+        res=" (${res})"
 	fi
+    echo -n "${res}"
 }
 
 
